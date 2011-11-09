aea4afcefe3eb8298714a90708b5dabdeed5a4d3
Christopher Gervais <chris@ergonlogic.com>
2011-11-09 11:31:41 -0500

    added queue_runner.pp and moved the rest of contrib into it's own .pp

---
+++ modules/aegir/manifests/contrib.pp
@@ -0,0 +1,44 @@
+class aegir::contrib {
+
+  drush::dl { ['hosting_queue_runner',
+               'hosting_platform_pathauto',
+               'hosting_backup_queue',
+               'hosting_backup_gc',
+               'hosting_upload',
+               'hosting_server_titles',
+               'aegir_rules',
+               #'hosting_advanced_cron',
+               'hosting_notifications',
+               #'provision_boost',
+               'sitedir_migrate',
+              ]:
+    site_alias => '@hostmaster',
+    cwd => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
+    require => Class['aegir'],
+  }
+
+  drush::en { ['hosting_queue_runner',
+               'hosting_platform_pathauto',
+               'hosting_backup_queue',
+               'hosting_backup_gc',
+               'hosting_upload',
+               'hosting_server_titles',
+               'aegir_rules',
+               #'hosting_advanced_cron',
+               #'hosting_notifications',
+               #'provision_boost',
+               'sitedir_migrate',
+              ]:
+    site_alias => '@hostmaster',
+    cwd => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
+    require => Class['aegir'],
+  }
+
+  drush::dis { ['sitedir_migrate',
+               ]:
+    site_alias => '@hostmaster',
+    cwd => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
+    require => Class['aegir'],
+  }
+
+}
---
+++ modules/aegir/manifests/queue_runner.pp
@@ -0,0 +1,36 @@
+class aegir::queue_runner {
+
+  include drush
+
+  drush::dl { 'hosting_queue_runner':
+    site_alias => '@hostmaster',
+    cwd => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
+    require => Class['aegir'],
+  }
+
+  drush::en { 'hosting_queue_runner':
+    site_alias => '@hostmaster',
+    cwd => "/var/aegir/hostmaster-6.x-1.5/sites/aegir.local",
+    require => [ Class['aegir'],
+                 Drush::Dl["hosting_queue_runner"],
+               ],
+  }
+
+  exec {"cp-script":
+    path => '/usr/bin:/bin',
+    command => "cp /var/aegir/hostmaster-6.x-1.5/sites/aegir.local/modules/hosting_queue_runner/init.d.example /etc/init.d/hosting-queue-runner",
+    require => Drush::Dl["hosting_queue_runner"],
+  }
+
+  exec {"symlinks-runlevels":
+    command => "/usr/sbin/update-rc.d hosting-queue-runner defaults",
+    require => Exec["cp-script"],
+  }
+
+  exec {"run-script":
+    path => '/usr/bin:/bin',
+    command => "/etc/init.d/hosting-queue-runner start",
+    require => Exec["symlinks-runlevels"],
+  }
+
+}
