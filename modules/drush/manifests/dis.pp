f1c0b702c0218a0427d6f8e62a014e74d5b0c0aa
Christopher Gervais <chris@ergonlogic.com>
2011-11-09 08:34:24 -0500

    add drush::dis definition

---
+++ modules/drush/manifests/dis.pp
@@ -0,0 +1,12 @@
+define drush::dis ($site_alias = "", $cwd = "/var/aegir") {
+
+  exec {"drush-en-${name}":
+    path => '/usr/bin:/bin',
+    user  => 'aegir',
+    group => 'aegir',
+    command => "drush ${site_alias} dis ${name} -y >> /var/aegir/drush.log 2>&1",
+    environment => "HOME=/var/aegir",
+    cwd => $cwd,
+  }
+                          
+}
