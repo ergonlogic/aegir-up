6bb37d39c15922c95ed29c04abdb3129cb3acb9d
Christopher Gervais <chris@ergonlogic.com>
2011-11-09 08:33:54 -0500

    fixed logging for dl

--- modules/drush/manifests/dl.pp
+++ modules/drush/manifests/dl.pp
@@ -4,7 +4,7 @@ define drush::dl ($site_alias = "", $cwd = "/var/aegir") {
     path => '/usr/bin:/bin',
     user  => 'aegir',
     group => 'aegir',
-    command => "drush ${site_alias} dl ${name} -y > /var/aegir/drush.log",
+    command => "drush ${site_alias} dl ${name} -y  >> /var/aegir/drush.log 2>&1",
     environment => "HOME=/var/aegir",
     cwd => $cwd,
   }
